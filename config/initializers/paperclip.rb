0b64bf8ff90c47b258acd9da5ad65639f60828c9
Christian Kakesa <christian.kakesa@gmail.com>
2016-02-15 00:59:41 +0100

    Fix paperclip incompatibility with aws-sdk > 2.0

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,7 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip.interpolates(:s3_eu_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-eu-central-1.amazonaws.com/#{attachment.bucket_name}/screenshots/#{attachment.path(style).gsub(%r{^/}, '')}"
+}
+
+Paperclip::Attachment.default_options[:url] = ':s3_eu_url'
 Paperclip::Attachment.default_options[:path] = 'screenshots/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3.eu-central-1.amazonaws.com'
