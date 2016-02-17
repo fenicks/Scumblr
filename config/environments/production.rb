8001938c4adce3d1a36938add3bf1d69d252b208
Christian Kakesa <christian.kakesa@gmail.com>
2016-02-17 03:47:21 +0100

    Update SMTP config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,14 @@ Scumblr::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              ENV['SES_HOST'] || 'localhost',
+      address:              ENV['SES_HOST'] || 'localhost'.freeze,
       port:                 Integer(ENV['SES_PORT'] || 587),
-      domain:               ENV['SES_DOMAIN'] || 'kakesa.net',
+      domain:               ENV['SES_DOMAIN'] || 'kakesa.net'.freeze,
       user_name:            ENV['SES_USERNAME'],
       password:             ENV['SES_PASSWORD'],
-      authentication:       ENV['SES_AUTHENTICATION_METHOD'] || 'plain',
+      authentication:       (ENV['SES_AUTHENTICATION_METHOD'] || 'plain'.freeze).to_sym,
       enable_starttls_auto: 'true'.eql?(ENV['SES_START_TLS_AUTO']) ? true : false,
-      openssl_verify_mode:  ENV['SES_SSL_VERIFY_MODE'] || 'none'
+      openssl_verify_mode:  ENV['SES_SSL_VERIFY_MODE'] || 'none'.freeze
   }
 
   config.paperclip_defaults = {
@@ -92,7 +92,7 @@ Scumblr::Application.configure do
       s3_credentials: {
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           bucket: ENV['AWS_S3_BUCKET_NAME'],
-          s3_protocol: (ENV['SCUMBLR_URL_DEFAULT_SCHEME'] || 'http').to_sym,
+          s3_protocol: (ENV['SCUMBLR_URL_DEFAULT_SCHEME'] || 'http'.freeze).to_sym,
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
       }
   }
