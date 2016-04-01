062ddf76b60c76b509650594bdb905de8255d754
Christian Kakesa <christian.kakesa@gmail.com>
2016-04-01 11:05:46 +0200

    Try to fix SMTP email sending with Amazon SES service.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Scumblr::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.default_url_options = { :host => "localhost:3000", :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => "localhost:3000", :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   Rails.application.routes.default_url_options[:host] = "localhost:3000"
